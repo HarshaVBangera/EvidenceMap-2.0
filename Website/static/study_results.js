ec54c881446370eedb4a053ebda65cf5583f4cdc
HarshaVBangera <156854856+HarshaVBangera@users.noreply.github.com>
2025-01-30 17:49:39 -0500

    Add files via upload
    This file now has the capability to take the collected level3 data from the flask route. It creates node and edges accordingly

--- Website/static/study_results.js
+++ Website/static/study_results.js
@@ -44,9 +44,16 @@ function draw_study_result(js_results_data, div_id, colors) {
     };
 
     // draw network
+    let idMapping = {};
     for (let i = 0; i < js_results_data['study_results'].length; i++) {
         let node = js_results_data['study_results'][i];
-        let node_id = node['label'];
+        let node_id
+        if (div_id === "collective_visualization") {
+            node_id = `doc${Math.floor(i/6)}_node${i}_${node['label']}`;
+            idMapping[node['label']] = node_id;
+        } else {
+            node_id = node['label']; 
+        }
         let node_type = node['type'];
         let node_label = node['term'];
 
@@ -70,12 +77,20 @@ function draw_study_result(js_results_data, div_id, colors) {
             if (from_id === "Root") {
                 continue;
             }
+            if (div_id === "collective_visualization") {
+                // Update the from_id to match the new node ID format
+                from_id = idMapping[from_id];
+            }
             if (!edgeExists(edges_des, from_id, node_id)) {
                 edges_des.push({from: from_id, to: node_id, color: 'gray'});
             }
         }
         for (let j = 0; j < node['connects_to'].length; j++) {
             let to_id = node['connects_to'][j];
+            if (div_id === "collective_visualization") {
+                // Update the to_id to match the new node ID format
+                to_id = idMapping[to_id];
+            }
             if (!edgeExists(edges_des, node_id, to_id)) {
                 edges_des.push({from: node_id, to: to_id, color: 'gray'});
             }
@@ -124,9 +139,18 @@ function draw_study_result(js_results_data, div_id, colors) {
         var container_res_popup = document.getElementById("study_results_popup");
         if (js_results_data['study_results'].length === 0) {
             container_res_popup.innerHTML = "<p style='display: flex; justify-content: center; align-items: center; height: 100%;'>No study results found</p>";
-        } else {
+        } 
+        else {
             options.nodes.font.size = 14;
             network_results_popup = new vis.Network(container_res_popup, data_res, options);
+        }
+    } else if (div_id === "collective_visualization") {
+        var container = document.getElementById("collective_visualization");
+        if (js_results_data['study_results'].length === 0) {
+            container.innerHTML = "<p style='display: flex; justify-content: center; align-items: center; height: 100%;'>No study results found</p>";
+        } else {
+            options.nodes.font.size = 14;
+            network_results = new vis.Network(container, data_res, options);
         }
     }
 }
@@ -139,13 +163,27 @@ var btn_res = document.getElementById("myBtn_result");
 // Get the <span> element that closes the modal
 var span_res = document.getElementsByClassName("close")[2];
 
-btn_res.onclick = function () {
-    modal_res.style.display = "block";
+if (btn_res) {
+    btn_res.onclick = function() {
+        modal_res.style.display = "block";
+    }
 }
 
 // When the user clicks on <span> (x), close the modal
 span_res.onclick = function () {
     modal_res.style.display = "none";
+}
+
+var modal_collective = document.getElementById("myModal_collective");
+var btn_collective = document.getElementById("myBtn_collective");
+var span_collective = document.getElementsByClassName("close")[3];
+
+btn_collective.onclick = function() {
+    modal_collective.style.display = "block";
+}
+
+span_collective.onclick = function() {
+    modal_collective.style.display = "none";
 }
 
 // window.addEventListener("load", () => {
